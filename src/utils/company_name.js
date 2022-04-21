e8148ababe40a677ce31ecb2d9d55b8c3d3fee35
layintondev <layintondev@gmail.com>
2022-04-21 15:53:26 +0100

    company details refactor and date fixes

--- src/utils/company_name.js
+++ src/utils/company_name.js
@@ -1,4 +1,4 @@
-export const COMPANY_NAME = " Binfat Computer Technology"
-export const COMPANY_EMAIL = " binfatstocksoftware@gmail.com"
-export const COMPANY_ADDRESS = "Union Bank Ikirun Osun State."
-export const COMPANY_PHONE = " +234-70-604-67430"
+export const COMPANY_NAME = "Adeshex Global"
+export const COMPANY_EMAIL = "Adeshexglobalconcept@gmail.com"
+export const COMPANY_ADDRESS = "Shop 1 and 2 oranmiayan tower lagere ile ife."
+export const COMPANY_PHONE = " +2348107895502"
