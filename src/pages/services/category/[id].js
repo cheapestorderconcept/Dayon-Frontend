2b11f6e76ca14af28d24befe2527098ef2eeca24
layintondev <layintondev@gmail.com>
2022-04-27 18:07:22 +0100

    services implementation

--- src/pages/services/category/[id].js
+++ src/pages/services/category/[id].js
@@ -1,8 +1,7 @@
 import Head from "next/head";
 import { Box, Container } from "@mui/material";
 import { DashboardLayout } from "../../../components/dashboard-layout";
-import { ProductBrand, ServiceCategory } from "src/components/serviceCategory/ServiceCategory";
-import BrandTable from "src/components/serviceCategory/brand-list";
+import {  ServiceCategory } from "src/components/serviceCategory/ServiceCategory";
 import dynamic from "next/dynamic";
 import { getBrands } from "src/statesManagement/store/actions/brand-action";
 import { useContext, useEffect, useState } from "react";
