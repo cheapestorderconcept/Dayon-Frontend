7cef5e17a7182d29b2c0d6864ef04bbc1b196ca4
layintondev <layintondev@gmail.com>
2022-05-18 07:41:11 +0100

    Fix payment bug

--- src/components/ServicePayment/add-service.js
+++ src/components/ServicePayment/add-service.js
@@ -87,7 +87,7 @@ export const AddService = (props) => {
         total_amount : `${values.total_amount}`,
    }
   
-  //  addServicePayment({dispatch, enqueueSnackbar, service:service}) 
+   addServicePayment({dispatch, enqueueSnackbar, service:service}) 
 
   };
   const removeservice = (values, setValues) => {
