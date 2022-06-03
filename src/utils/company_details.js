2954a50ed2d0b876b74ceb28a1cea985bde14814
layintondev <layintondev@gmail.com>
2022-06-03 13:28:37 +0100

    Solace initial commit

--- src/utils/company_details.js
+++ src/utils/company_details.js
@@ -1,5 +1,5 @@
 export const COMPANY_NAME = "Solace Medicare hospital."
 export const COMPANY_EMAIL = "Solacehospital22@gmail.com"
-export const COMPANY_ADDRESS = "ile ife osun state"
-export const COMPANY_PHONE = "+234-111-111-111"
+export const COMPANY_ADDRESS = "Modakeke, Osun State."
+export const COMPANY_PHONE = "08035992252"
 export const COMPANY_INITIALS = "SOC"
