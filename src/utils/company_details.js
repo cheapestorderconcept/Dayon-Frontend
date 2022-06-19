4ab0e106036eae1c18512813993298b17a942259
layintondev <layintondev@gmail.com>
2022-06-19 19:25:35 +0100

    address fixes

--- src/utils/company_details.js
+++ src/utils/company_details.js
@@ -1,5 +1,5 @@
 export const COMPANY_NAME = "Solace Medicare hospital.";
 export const COMPANY_EMAIL = "Solacehospital22@gmail.com";
-export const COMPANY_ADDRESS = "Modakeke, Osun State.";
-export const COMPANY_PHONE = "08035992252";
+export const COMPANY_ADDRESS = "Adeoye Estate, Gbalefefe Modakeke, Osun State.";
+export const COMPANY_PHONE = "08093960909, 08093960912";
 export const COMPANY_INITIALS = "SOC";
