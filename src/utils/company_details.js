dc21132c0070eef8fcf9cb2f37189d5326f510c9
LayintonDev <layintondev@gmail.com>
2023-05-03 17:29:11 +0100

    fumbol commit

--- src/utils/company_details.js
+++ src/utils/company_details.js
@@ -1,5 +1,5 @@
-export const COMPANY_NAME = "Awetco Stores";
-export const COMPANY_EMAIL = "Awetco00@gmail.com";
-export const COMPANY_ADDRESS = "No. 180, Hezekiah Oluwasanmi Rd7, Ile-Ife, Osun State.";
-export const COMPANY_PHONE = " +2347032529199, +2348060410828, +2348117729805.";
-export const COMPANY_INITIALS = "AWE";
+export const COMPANY_NAME = "Fumbol Glory Ventures";
+export const COMPANY_EMAIL = "fumbol@gmail.com";
+export const COMPANY_ADDRESS = "Onireke street off oja tutu market Opposite Chris Fisherman church Ile Ife, Osun State.";
+export const COMPANY_PHONE = " 07032272406, 08066060369.";
+export const COMPANY_INITIALS = "FGV";
